f24225cf10fecab4c9346d40abeee0dc16d8135a
yuxian <yx10177@gmail.com>
2022-04-12 23:46:28 +0800

    Add First UnitTest

---
+++ TraceBackend/Trace.UnitTest/TraceApiTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+
+using Trace.BLL;
+using Trace.Model;
+using Trace.Repository;
+
+namespace Trace.UnitTest
+{
+    [TestFixture]
+    public class TraceApiTests
+    {
+        
+
+        [Test]
+        [TestCase("testAccount", "")]
+        [TestCase("", "testpassword")]
+        [TestCase("", "")]
+        public void Register_LoginArgs_ReturnFail(string account, string password) 
+        {
+            // Arrange
+            var mockUser = new Mock<IUserRepository>();
+            var mockUserFriend = new Mock<IUserFriendRepository>();
+            UserCenter userCenter = new UserCenter(mockUser.Object, mockUserFriend.Object);
+            // Act
+            var task = userCenter.Register(new LoginArgs { Account = account, Password = password });
+            // Assert
+            Assert.AreEqual(EnumStatusCode.Fail,task.Result.StatusCode );
+        }
+    }
+}
